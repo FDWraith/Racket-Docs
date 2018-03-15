21828bdd6ea3c837c8907b693db4de153158b4b8
jakob <jakobeha@gmail.com>
2018-03-15 19:32:03 -0400

    phase-1 docs

--- lib/racket-docs/utils/parse-class.rkt
+++ lib/racket-docs/utils/parse-class.rkt
@@ -7,8 +7,11 @@
 
 (require [for-syntax "syntax.rkt"
                      syntax/parse
-                     racket/syntax]
-         syntax/parse)
+                     racket/syntax
+                     racket/function]
+         syntax/parse
+         [for-template syntax/parse
+                       racket])
 
 ; Parse class - A syntax class which both matches an expression and parses it.
 ; Functions as both define-syntax-class (match) and define-syntax (parse).
@@ -19,15 +22,20 @@
       [(_ id:id
           stxclass-option:stxparse-option ...
           [in-match pattern-option:pattern-option ... out-expr ...+] ...+)
+       #:with define-gen-syntax-class define-gen-syntax-class-stx
        #:with (stxclass-opt-part ...) (flatten/stx #'(stxclass-option ...))
        #:with ((pattern-opt-part ...) ...)
        (map/stx flatten/stx #'((pattern-option ...) ...))
-       #`(#,define-gen-syntax-class-stx
-          id
-          stxclass-opt-part ...
-          [pattern in-match
-                   pattern-opt-part ...
-                   #:attr out (begin out-expr ...)] ...)])))
+       #:with id1 (syntax-shift-phase-level #'id -1)
+       #'(define-gen-syntax-class id
+           stxclass-opt-part ...
+           [pattern in-match
+                    pattern-opt-part ...
+                    #:attr out (begin out-expr ...)
+                    #:attr out1
+                    #`(syntax-parse #'#,this-syntax
+                        ; TODO Fix ellipses, then use in-match directly
+                        [(~var x id1) (attribute x.out)])] ...)])))
 
 (define-syntax define-parse-class
   (define-gen-parse-class #'define-syntax-class))
@@ -38,11 +46,11 @@
 (define-syntax parse-class
   (syntax-parser
     [(_ id:id)
-     #:with id.out (format-id #'id "~a.out" (syntax-e #'id))
+     #:with id.out (format-id #'id #:source #'id "~a.out" (syntax-e #'id))
      #'(attribute id.out)]))
 
 (define-syntax parse-classes
   (syntax-parser
     [(_ (id:id (~literal ...)))
-     #:with id.out (format-id #'id "~a.out" (syntax-e #'id))
+     #:with id.out (format-id #'id #:source #'id "~a.out" (syntax-e #'id))
      #'(attribute id.out)]))
