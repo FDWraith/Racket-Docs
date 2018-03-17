8e46174a7dcf0d4695a867d8171a74f0436c7e48
Kevin Zhang <kevinzhang3702@gmail.com>
2018-03-17 17:35:34 -0400

    phases are screwing me over, need to rethink approach

--- lib/racket-docs/compile.rkt
+++ lib/racket-docs/compile.rkt
@@ -1,12 +1,17 @@
-#lang typed/racket
+#lang racket
 
+(require (for-syntax "struct.rkt"
+                     "utils.rkt"
+                     racket
+                     syntax/parse))
 (require "struct.rkt")
-(require/typed "struct.rkt"
-               [#:struct doc-entry [(type : (U 'type 'macro 'value)) (id : Any) (props : [Listof doc-prop])]]
-               )
-(require/typed "utils.rkt"
-               [syntax->string [Any -> String]]
-               )
+(require "utils.rkt")
+#;(require/typed "struct.rkt"
+                 [#:struct doc-entry [(type : (U 'type 'macro 'value)) (id : Any) (props : [Listof doc-prop])]]
+                 )
+#;(require/typed "utils.rkt"
+                 [syntax->string [Any -> String]]
+                 )
 
 #|
 ;; Figure out where this compile-time function is being called from ???
@@ -16,19 +21,30 @@
 |#
 
 ; Effect: Generates a Scribble File from the documentation
-(: compile-docs (-> (Listof doc-entry) Void))
-(define (compile-docs all-docs)
-  (local
-    ((define out (open-output-file "hello.scrbl" #:exists 'truncate/replace)))
-    (begin
-      (display "#lang scribble/manual" out)
-      (display (string-join (map compile-doc-entry all-docs) "\n") out)
-      (close-output-port out))))
+; (: compile-docs (-> (Listof doc-entry) Void))
+(define-syntax (compile-docs stx)
+  (syntax-parse stx
+    [(_ all-docs)
+     #'(begin
+         (define out (open-output-file "hello.scrbl" #:exists 'truncate/replace))
+         (display "#lang scribble/manual" out)
+         (display (string-join (map compile-doc-entry all-docs) "\n") out)
+         (close-output-port out))]))
 
 
 ; DocEntry -> String
 ; Compiles the document entry to valid Scribble line(s)
-(: compile-doc-entry (-> doc-entry String))
+; (: compile-doc-entry (-> doc-entry String))
+#;(define-syntax (compile-doc-entry stx)
+    (syntax-parse stx
+      [(_ ent)
+       #'(begin
+           (define ent-type (doc-entry-type ent))
+           (cond
+             [(symbol=? ent-type 'type) (compile-doc-data ent)]
+             [(symbol=? ent-type 'value) (compile-doc-const ent)]
+             [(symbol=? ent-type 'macro) (compile-doc-func ent)]))]
+      [else (displayln stx)]))
 (define (compile-doc-entry ent)
   (local
     ((define ent-type (doc-entry-type ent)))
@@ -39,7 +55,7 @@
 
 ; DocEntry -> String
 ; Compiles Data Definitions to valid Scribble line(s)
-(: compile-doc-data (-> doc-entry String))
+; (: compile-doc-data (-> doc-entry String))
 (define (compile-doc-data dat)
   (local
     ((define dat-type (doc-entry-id dat))
@@ -61,35 +77,35 @@
 
 ; DocEntry -> String
 ; Compiles Constants to valid Scribble line(s)
-(: compile-doc-const (-> doc-entry String))
-(define (compile-doc-const ent)
-  "")
+; (: compile-doc-const (-> doc-entry String))
+(define-syntax (compile-doc-const ent)
+  #'"")
 
 ; DocEntry -> String
 ; Compiles Functions to valid Scribble line(s)
-(: compile-doc-func (-> doc-entry String))
-(define (compile-doc-func ent)
-  "")
+; (: compile-doc-func (-> doc-entry String))
+(define-syntax (compile-doc-func ent)
+  #'"")
 
 ; DocEntry -> String
 ; Compiles Macros to valid Scribble line(s)
-(: compile-doc-macro (-> doc-entry String))
-(define (compile-doc-macro ent)
-  "")
+; (: compile-doc-macro (-> doc-entry String))
+(define-syntax (compile-doc-macro ent)
+  #'"")
 
 
 ; DocPropType -> [DocProp -> Boolean]
 ; Creates a function that determines if a given DocProp
 ; matches that type
-(: mk-prop? (-> Doc-Prop-Type [-> doc-prop Boolean]))
-(define (mk-prop? type )
+; (: mk-prop? (-> Doc-Prop-Type [-> doc-prop Boolean]))
+(define (mk-prop? type)
   (λ (prop) (prop-type=? type (doc-prop-type prop))))
 
   
 ; [doc-prop -> Boolean] [Listof doc-prop] -> [U doc-prop '()]
 ; Returns the first element in the list that matches pred
 ; Returns an empty list if no such element is found
-(: extract (-> (-> doc-prop Boolean) [Listof doc-prop] (U doc-prop '())))
+; (: extract (-> (-> doc-prop Boolean) [Listof doc-prop] (U doc-prop '())))
 (define (extract pred lst)
   (cond
     [(empty? lst) lst]
