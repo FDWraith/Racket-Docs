5acdc2417962f7ad7ea309d3a42b173c7e4099f6
Kevin Zhang <kevinzhang3702@gmail.com>
2018-03-26 00:26:03 -0400

    going to merge

--- lib/racket-docs/compile.rkt
+++ lib/racket-docs/compile.rkt
@@ -45,18 +45,35 @@
                      (doc-prop-value desc-prop)))
     (define example-prop? (mk-prop? 'examples))
     (define example-prop (extract example-prop? dat-body))
-    (string-append "Data:" dat-type "\n"
-                   "Types:\n"
-                   type "\n"
-                   "Interpretation:" desc)))
+    (string-append "@defthing["dat-type " data-def? #:value" 
+                   "(code:line " type "]{\n"
+                   desc "\n}")))
 
 ; Compiles Functions to valid Scribble line(s)
 (define (compile-doc-func stx)
   "function string here")
 
 ; Compiles Constants to valid Scribble line(s)
-(define (compile-doc-const stx)
-  "something string here")
+(define (compile-doc-const ent)
+  (begin
+    (define name (syntax->string (doc-entry-id ent)))
+    (define props (doc-entry-props ent))
+    (define args-prop? (mk-prop? 'args))
+    (define args-prop (extract args-prop? props))
+    (define type-prop? (mk-prop? 'type))
+    (define type-prop (extract type-prop? props))
+    (define desc-prop? (mk-prop? 'desc))
+    (define desc-prop (extract desc-prop? props))
+    ;; TODO: Match each argument to its corresponding signature part (figure this out)
+    (define sig
+      (cond
+        [(doc-prop? type-prop) (syntax->string (doc-prop-value type-prop))]
+        [else (error "signature not found")]))
+    (define purp
+      (cond
+        [(doc-prop? desc-prop) (doc-prop-value desc-prop)]
+        [else (error "purpose not found")]))
+    "@defproc["))
 
 ; Compiles Macros to valid Scribble line(s)
 (define (compile-doc-macro stx)
